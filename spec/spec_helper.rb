35ed27ebe7e96c14e3a4f25db2864d2704aa582c
Ryan Fitzgerald <fitz@Ryan-Fitzgeralds-MacBook-Pro-2.local>
2010-07-26 17:22:09 -0400

    added rakefile to run tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
 $:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
+require 'rspec'
+# optionally add autorun support
+require 'rspec/autorun'
+
+Rspec.configure do |c|
+  c.mock_with :rspec
+end
+
 require 'mongo_backer'
